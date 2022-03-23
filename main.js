cc9590e371ea9a1cecb3c83070dbd71b94b525dc
evsem <frukwork@gmail.com>
2022-03-23 19:24:18 +0300

    v_3(startTimer)

--- main.js
+++ main.js
@@ -8,9 +8,54 @@ const pauseButton = document.querySelector('.pause')
 const stopButton = document.querySelector('.stop')
 
 let hours = 00,
-  minute = 00,
+  minutes = 00,
   seconds = 00,
-  milliseconds = 00
-interval
+  milliseconds = 00,
+  interval
 
-function startTimer() {}
+startButton.addEventListener('click', () => {
+  clearInterval(interval)
+  interval = setInterval(startTimer, 10)
+})
+
+function startTimer() {
+  milliseconds++
+  if (milliseconds < 9) {
+    millisecondElement.innerText = '0' + milliseconds
+  }
+  if (milliseconds > 9) {
+    millisecondElement.innerText = milliseconds
+  }
+  if (milliseconds > 99) {
+    seconds++
+    secondElement.innerText = '0' + seconds
+    milliseconds = 0
+    millisecondElement.innerText = '0' + milliseconds
+  }
+
+  if (seconds < 9) {
+    secondElement.innerText = '0' + seconds
+  }
+  if (seconds > 9) {
+    secondElement.innerText = seconds
+  }
+  if (seconds > 59) {
+    minutes++
+    minuteElement.innerText = '0' + minutes
+    seconds = 0
+    secondElement.innerText = '0' + seconds
+  }
+
+  if (minutes < 9) {
+    minuteElement.innerText = '0' + minutes
+  }
+  if (minutes > 9) {
+    minuteElement.innerText = minutes
+  }
+  if (minutes > 59) {
+    hours++
+    hourElement.innerText = '0' + hours
+    minutes = 0
+    minuteElement.innerText = '0' + minutes
+  }
+}
