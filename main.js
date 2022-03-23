2594f7aebf34b80929dc8c0fe3ca575c9e1589e6
evsem <frukwork@gmail.com>
2022-03-23 18:47:41 +0300

    v_2(startJS)

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hello')
+const hourElement = document.querySelector('.hours')
+const minuteElement = document.querySelector('.minutes')
+const secondElement = document.querySelector('.seconds')
+const millisecondElement = document.querySelector('.milliseconds')
+
+const startButton = document.querySelector('.start')
+const pauseButton = document.querySelector('.pause')
+const stopButton = document.querySelector('.stop')
+
+let hours = 00,
+  minute = 00,
+  seconds = 00,
+  milliseconds = 00
+interval
+
+function startTimer() {}
